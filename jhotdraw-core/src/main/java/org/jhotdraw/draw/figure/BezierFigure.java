0016ad64b2a08731bea9c6aab7c8d65dcf8a0096
jaykay99992 <jasemkhelifi@gmail.com>
2024-02-06 21:34:11 +0100

    added else statement, and reduced the number of returns

--- jhotdraw-core/src/main/java/org/jhotdraw/draw/figure/BezierFigure.java
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/figure/BezierFigure.java
@@ -171,11 +171,12 @@ public class BezierFigure extends AbstractAttributedFigure {
 
   @Override
   public boolean contains(Point2D.Double p, double scaleDenominator) {
+    boolean result = false;
     double tolerance =
         Math.max(1f, 2 * AttributeKeys.getPerpendicularHitGrowth(this, scaleDenominator));
     if (isClosed() || attr().get(FILL_COLOR) != null && attr().get(UNCLOSED_PATH_FILLED)) {
       if (path.contains(p)) {
-        return true;
+        result = true;
       }
       double grow = tolerance;
       GrowStroke gs =
